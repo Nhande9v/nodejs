9f3251a37550697070d36d46ee6d13a60069c083
Nhan Nguyen <Hoangnhan912004dn@gmail.com>
2025-07-02 21:23:22 +0700

    fix1

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 async function connect(){
- 
+    const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/phongtro_dev';
     try {
-        await mongoose.connect('mongodb://localhost:27017/phongtro_dev');  
+        await mongoose.connect(mongoUri);  
         console.log("Connect succesfully!!!");
     } catch (error) {
         console.log("Connect fail!!!");
     }
-
 }
 
 module.exports = { connect };
